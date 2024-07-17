cdbeed971e75536c2d74d58bf06cff39f128da49
Carolina Floro <carolinarochaf@hotmail.com>
2024-07-17 15:25:56 -0300

    Style: projects and dialog

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,64 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+
 import { IProjects } from '../../interface/projects.interface';
+
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+import { EDialogPanelClass } from '../../enum/e-dialog-panel-class.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
+
   public projectsArray = signal<IProjects[]>([
     {
       src: 'img\\project.png',
       alt: '',
-      title: 'título do projeto',
+      title: 'Título do projeto',
       width: '100px',
       height: '51px',
-      description: 'descrição do projeto',
-      links: [
-        {
-          name: 'Visite',
-          href: 'link',
-        },
-      ],
-    },
-    {
-      src: 'img\\project.png',
-      alt: '',
-      title: 'título do projeto',
-      width: '100px',
-      height: '51px',
-      description: 'descrição do projeto',
-      links: [
-        {
-          name: 'Visite',
-          href: 'link',
-        },
-      ],
-    },
-    {
-      src: 'img\\project.png',
-      alt: '',
-      title: 'título do projeto',
-      width: '100px',
-      height: '51px',
-      description: 'descrição do projeto',
-      links: [
-        {
-          name: 'Visite',
-          href: 'link',
-        },
-      ],
-    },
-    {
-      src: 'img\\project.png',
-      alt: '',
-      title: 'título do projeto',
-      width: '100px',
-      height: '51px',
-      description: 'descrição do projeto',
+      description:
+        'Aqui uma descrição mais completa do projeto, com objetivo, arquitetura e tecnologias utilizadas.',
       links: [
         {
           name: 'Visite',
@@ -67,4 +34,11 @@ export class ProjectsComponent {
       ],
     },
   ]);
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS,
+    });
+  }
 }
